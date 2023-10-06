a10844d3d0b2708c327027af9b2dc0e0181dcd5a
Alen Zgurić <zguric.alen@hotmail.com>
2023-10-06 13:41:31 +0200

    add copyright componenet

---
+++ src/components/footer/js/CopyRight.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+function CopyrightYear() {
+  const [currentYear, setCurrentYear] = useState(0); // Početna vrijednost 0, bit će zamijenjena prilikom montiranja
+  const [copyrightText, setCopyrightText] = useState('');
+
+  useEffect(() => {
+    // Funkcija za dohvaćanje trenutne godine
+    const getCurrentYear = () => {
+      const date = new Date();
+      return date.getFullYear();
+    };
+
+    // Postavljamo trenutnu godinu kada se komponenta montira
+    setCurrentYear(getCurrentYear());
+
+    const yearDifference = currentYear > 2023 ? `2023 - ${currentYear}` : '2023';
+    setCopyrightText(`Copyright ® ${yearDifference}. CryptoText `);
+  }, [currentYear]);
+
+  return (
+    <div>
+      <span>{copyrightText}</span>
+    </div>
+  );
+}
+
+export default CopyrightYear;
