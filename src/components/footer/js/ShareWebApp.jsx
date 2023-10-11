770f188847039a00abf025f3c564a1232e5d5570
Alen Zgurić <zguric.alen@hotmail.com>
2023-10-11 12:19:01 +0200

    add share component icons

--- src/components/footer/js/ShareWebApp.jsx
+++ src/components/footer/js/ShareWebApp.jsx
@@ -1,54 +1,38 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaLinkedin, FaViber, FaWhatsapp } from "react-icons/fa";
-import "../css/ShareWebAppStyle.css";
+import {
+  FacebookShareButton,
+  ViberShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  ViberIcon,
+  WhatsappIcon,
+  EmailIcon,
+} from "react-share";
 
-const ShareWebApp = () => {
-  const location = useLocation();
-  const currentUrl = location.pathname;
-
-  // Funkcija za dijeljenje na Facebooku
-  const shareOnFacebook = () => {
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`, '_blank');
-  };
-
-  // Funkcija za dijeljenje na Instagramu
-  const shareOnInstagram = () => {
-    // URL adresa s pripremljenim sadržajem za objavu
-    const instagramShareLink = `https://www.instagram.com/share?url=${currentUrl}`;
-    window.open(instagramShareLink, '_blank');
-  };
-  
-
-  // Funkcija za dijeljenje na LinkedIn-u
-  const shareOnLinkedin = () => {
-    window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${currentUrl}`, '_blank');
-  };
-
-  // Funkcija za dijeljenje na Viberu
-  const shareOnViber = () => {
-    // URL adresa s pripremljenim sadržajem za dijeljenje
-    const viberShareLink = `viber://forward?text=${encodeURIComponent(currentUrl)}`;
-    window.open(viberShareLink);
-  };
-  
-  
-
-  // Funkcija za dijeljenje na WhatsApp-u
-  const shareOnWhatsapp = () => {
-    window.open(`https://api.whatsapp.com/send?text=${currentUrl}`, '_blank');
-  };
+const ShareComponent = () => {
+  const currentUrl = window.location.href; // Ili location.href ako želite cijeli URL
 
   return (
-    <div className="share-web-app">
-      <span>Podijeli:</span>
-      <FaFacebook onClick={shareOnFacebook} className="share-icon" />
-      <FaInstagram onClick={shareOnInstagram} className="share-icon" />
-      <FaLinkedin onClick={shareOnLinkedin} className="share-icon" />
-      <FaViber onClick={shareOnViber} className="share-icon" />
-      <FaWhatsapp onClick={shareOnWhatsapp} className="share-icon" />
+    <div>
+      <h2>Podijeli ovu stranicu:</h2>
+      <FacebookShareButton url={currentUrl}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+      <ViberShareButton url={currentUrl}>
+        <ViberIcon size={32} round />
+      </ViberShareButton>
+      <WhatsappShareButton url={currentUrl}>
+        <WhatsappIcon size={32} round />
+      </WhatsappShareButton>
+      <EmailShareButton
+        subject="Pogledajte ovu sjajnu web aplikaciju"
+        body={`Pogledajte ovu sjajnu web aplikaciju:\n\n${currentUrl}`}
+      >
+        <EmailIcon size={32} round />
+      </EmailShareButton>
     </div>
   );
 };
 
-export default ShareWebApp;
+export default ShareComponent;
